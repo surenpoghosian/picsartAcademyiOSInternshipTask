b5372c7b1606ab5a2178ff09804253264ff12e1b
surenpoghosian <56313895+surenpoghosian@users.noreply.github.com>
2023-11-22 22:53:02 +0400

    Initial Commit

---
+++ Picsart Academy iOS Task (Suren Poghosyan)/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Picsart Academy iOS Task (Suren Poghosyan)
+//
+//  Created by Suren Poghosyan on 22.11.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
